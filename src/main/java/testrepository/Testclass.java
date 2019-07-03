6ff7b8d61ff70fdf026d25a44c8a7c73208069bc
shanthigithub996 <shanthi.singireddi@gmail.com>
2019-07-03 14:08:22 +0530

    added new project

---
+++ src/main/java/testrepository/Testclass.java
@@ -0,0 +1,14 @@
+package testrepository;
+
+public class Testclass {
+	static String a = "Firstname";
+	static String b = "lastname";
+
+	public static void main(String[] args) {
+
+		String c = a + b;
+		System.out.println("fullname is; " + c);
+
+	}
+
+}
